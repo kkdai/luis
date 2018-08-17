a6e5b19c7df548105d7237e16fcaf98f1a19b6a3
Evan Lin <evanslin@gmail.com>
2018-08-18 01:27:55 +0800

    test

--- example_test.go
+++ example_test.go
@@ -1,13 +1,14 @@
-package luis
+package luis_test
 
 import (
 	"fmt"
 	"os"
 
+	"github.com/kkdai/luis"
 	"github.com/prometheus/common/log"
 )
 
-func ExamplePredict() {
+func Example() {
 	var API_KEY string
 	var APPID string
 
@@ -16,24 +17,22 @@ func ExamplePredict() {
 
 	if API_KEY == "" {
 		fmt.Println("Please export your key to environment first, `export SUB_KEY=12234 && export APP_ID=5678`")
-	}
-	if API_KEY == "" {
 		return
 	}
 
-	e := NewLuis(API_KEY, APPID)
+	e := luis.NewLuis(API_KEY, APPID)
 
 	res, err := e.IntelList()
-
 	if err != nil {
 		log.Error("Error happen on :", err.Err)
 	}
+
 	fmt.Println("Got response:", string(res))
-	result := NewIntentListResponse(res)
+	result := luis.NewIntentListResponse(res)
 	fmt.Println("Luis Intent Ret", result)
 
 	//Add utterances
-	ex := ExampleJson{ExampleText: "test", SelectedIntentName: "test2"}
+	ex := luis.ExampleJson{ExampleText: "test", SelectedIntentName: "test2"}
 	res, err = e.AddLabel(ex)
 
 	//Train it
@@ -49,5 +48,25 @@ func ExamplePredict() {
 		log.Error("Error happen on :", err.Err)
 	}
 	fmt.Println("Got response:", string(res))
-	fmt.Println("Get the best predict result:", GetBestScoreIntent(NewPredictResponse(res)))
+	fmt.Println("Get the best predict result:", luis.GetBestScoreIntent(luis.NewPredictResponse(res)))
+}
+
+func ExampleIntelList() {
+	var API_KEY string
+	var APPID string
+
+	APPID = os.Getenv("APP_ID")
+	API_KEY = os.Getenv("SUB_KEY")
+
+	if API_KEY == "" {
+		fmt.Println("Please export your key to environment first, `export SUB_KEY=12234 && export APP_ID=5678`")
+		return
+	}
+
+	e := luis.NewLuis(API_KEY, APPID)
+
+	_, err := e.IntelList()
+	if err != nil {
+		log.Error("Error happen on :", err.Err)
+	}
 }
